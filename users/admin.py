aa49d879542fa70ff52d306c027960a1d956393e
Codin-Go <codingo@icloud.com>
2022-07-24 02:57:20 +0900

    2.1.b intro to User Model

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+from . import models
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
